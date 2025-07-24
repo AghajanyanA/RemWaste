9e5455afa79a6b3e6e8594b750a86318569a56ee
Armen <74616019+AghajanyanA@users.noreply.github.com>
2025-07-24 09:56:56 +0400

    fix yml typo (#4)
    * fix yml typo
    
    * installed package
    
    * cd to ui test
    
    * change ui test dir
    
    * change ci run order
    
    * set dirs
    
    * trigger check
    
    * remove dir
    
    * edit locators, update ui-tests-yml
    
    * remove start backend manually

--- frontend/__ui__/src/pages/tasksPage/tasksPage.ts
+++ frontend/__ui__/src/pages/tasksPage/tasksPage.ts
@@ -4,32 +4,32 @@ import { Locator } from "@playwright/test";
 
 export class TasksPage extends Container {
     private LOCATORS = {
-        title: '//h2',
-        addButton: '//button[@data-testid="add-todo-button"]',
-        newTaskInput: '//input[@data-testid="new-todo-input"]',
-        todoList: '//ul[@data-testid="todo-list"]',
-        todoItem: '//li'
+        title: this.page.locator('//h2'),
+        addButton: this.page.locator('//button[@data-testid="add-todo-button"]'),
+        newTaskInput: this.page.locator('//input[@data-testid="new-todo-input"]'),
+        todoList: this.page.locator('//ul[@data-testid="todo-list"]'),
+        todoItem: this.page.locator('//li')
     }
 
-    public TodoItem = new TodoItem(this.LOCATORS.todoItem as unknown as Locator, this.page);
+    public TodoItem = new TodoItem(this.LOCATORS.todoItem, this.page);
 
     public async getTitle(): Promise<string> {
-        return this.page.locator(this.LOCATORS.title).innerText();
+        return this.LOCATORS.title.innerText();
     }
 
     public async getTodoListItems(): Promise<string[]> {
-        return this.page.locator(this.LOCATORS.todoList).locator('li').allInnerTexts();
+        return this.LOCATORS.todoList.locator('li').allInnerTexts();
     }
     
     public async clickAddButton(): Promise<void> {
-        await this.page.click(this.LOCATORS.addButton);
+        this.LOCATORS.addButton.click;
     }
 
     public async fillNewTaskInput(task: string): Promise<void> {
-        await this.page.fill(this.LOCATORS.newTaskInput, task);
+        await this.LOCATORS.newTaskInput.fill(task);
     }
 
     public async getTodoListCount(): Promise<number> {
-        return this.page.locator(this.LOCATORS.todoList).locator('li').count();
+        return this.LOCATORS.todoList.locator('li').count();
     }
 }
